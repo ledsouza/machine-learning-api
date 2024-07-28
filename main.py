a878a15c8b648a72e516d78da996f50093858464
ledsouza <leandro.souza.159@gmail.com>
2024-07-28 16:59:45 -0300

    criada a rota de sentimento

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask
 
+from textblob import TextBlob
+from googletrans import Translator
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def home():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/sentimento/<frase>")
+def sentimento(frase):
+    translator = Translator()
+    frase_en = translator.translate(frase)
+    tb = TextBlob(frase_en.text.replace("-", " "))
+    polaridade = tb.sentiment.polarity
+    return f"<h2>Polaridade: {polaridade}</h2>"
