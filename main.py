179b41a3d789f845cf4597966fd8e9f920961ad3
ledsouza <leandro.souza.159@gmail.com>
2024-07-28 17:53:05 -0300

    criando o endpoint para o modelo

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from flask import Flask
 from textblob import TextBlob
 from googletrans import Translator
 
+import pickle
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+def get_model() -> LinearRegression:
+    filename = 'modelo.pkl'
+    modelo = pickle.load(open(filename, 'rb'))
+    return modelo
+
+
+modelo = get_model()
+
 app = Flask(__name__)
 
 
@@ -18,3 +31,10 @@ def sentimento(frase):
     tb = TextBlob(frase_en.text.replace("-", " "))
     polaridade = tb.sentiment.polarity
     return f"<h2>Polaridade: {polaridade}</h2>"
+
+
+@app.route("/cotacao/<int:tamanho>")
+def cotacao(tamanho):
+    tamanho = np.array(tamanho).reshape(-1, 1)
+    preco = modelo.predict(tamanho)[0]
+    return f"{preco}"
